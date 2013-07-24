97a89b55ddbacd1a57b76877f1d1c8b3e0e0f42f
Arulprakash <arulprakash@tevatel.com>
2013-07-24 15:50:33 +0530

    modified for autosuggest

--- application/models/user.php
+++ application/models/user.php
@@ -6,7 +6,7 @@ Class User extends CI_Model {
  function checkLogin($username,$password) {
    $sql = "SELECT id,first_name,username FROM users WHERE username=? AND password=?";
    $query = $this->db->query($sql, array($username,md5($password)));      
-   return $query->result_array();   
+   return $query->result_array();
  }
  function getUserInformation($id){
    $sql = "SELECT * FROM users WHERE id=?";
@@ -14,7 +14,7 @@ Class User extends CI_Model {
    return $query->result_array(); 
  }
   function getAllAgents($q) {
-    $sql = "SELECT username from users where username like '" .$q."%'";
+    $sql = "SELECT distinct(agent) from queue_log where agent like '%$q%'";
     $query = $this->db->query($sql);
     return $query->result();
   } 
