3edf649c8195e2b41c8eabd402f760e9a487c320
kumuthavalli <kumuthavalli@tevatel.com>
2013-07-24 15:58:08 +0530

    commiting changes done in menu

--- application/views/menu.php
+++ application/views/menu.php
@@ -19,10 +19,15 @@
 		</li>
 		<li><a href="#">PBX Admin</a>
 			<ul>
-				<li><a href="#">Extensions</a></li>
-				<li><a href="#">Follow Me</a></li>
-				<li><a href="#">Queues</a></li>
-				<li><a href="#">Inbound</a></li>
+				<li><a href="#">Extensions</a>
+					<ul>
+							<li><a href="<? echo base_url().'index.php/pbx_admin/add_extension'?>">Add extension</a></li>
+							<li><a href="<? echo base_url().'index.php/pbx_admin/view_extension'?>">View extensions</a></li>
+					</ul>
+				</li>
+				<li><a href="<? echo base_url().'index.php/pbx_admin/follow_me'?>">Follow Me</a></li>
+				<li><a href="<? echo base_url().'index.php/pbx_admin/queue'?>">Queues</a></li>
+				<li><a href="<? echo base_url().'index.php/pbx_admin/inbound'?>">Inbound</a></li>
 			</ul>
 		</li>
 		<li><a href="#">Real Time Report</a></li>
