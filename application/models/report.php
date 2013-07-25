56ec3095018693d7099a967d06c797b67d9656e8
Arulprakash <arulprakash@tevatel.com>
2013-07-25 11:48:29 +0530

    added dialer,ivr and recordings

--- application/models/report.php
+++ application/models/report.php
@@ -22,12 +22,39 @@ $sql = "select calldate,clid,src,dst,disposition,sec_to_time(billsec) as b from 
 
 function queueReport(){
 
-$sql = "SELECT callid,queuename,TIMEDIFF((SELECT DATE_FORMAT( TIME,  '%H:%i:%s' ) from queue_log where callid=q1.callid and event='CONNECT') ,(SELECT DATE_FORMAT( TIME,  '%H:%i:%s' ) from queue_log where callid=q1.callid and event='ENTERQUEUE')) as Wait_Time,(select data2 from queue_log where event='ENTERQUEUE' and callid=q1.callid) as Callerid,agent,TIMEDIFF((SELECT DATE_FORMAT( TIME,  '%H:%i:%s' ) from queue_log where callid=q1.callid and (event='COMPLETECALLER' or event='COMPLETEAGENT')) ,(SELECT DATE_FORMAT( TIME,  '%H:%i:%s' ) from queue_log where callid=q1.callid and event='CONNECT')) as Talk_Time,DATE_FORMAT( TIME,  '%d-%m-%Y %H:%i:%s' ) as time from queue_log as q1 where event='CONNECT' and callid=q1.callid $sql2 $sql4 order by time desc";
+$sql = "SELECT callid,queuename,TIMEDIFF((SELECT DATE_FORMAT( TIME,  '%H:%i:%s' ) from queue_log where callid=q1.callid and event='CONNECT') ,(SELECT DATE_FORMAT( TIME,  '%H:%i:%s' ) from queue_log where callid=q1.callid and event='ENTERQUEUE')) as Wait_Time,(select data2 from queue_log where event='ENTERQUEUE' and callid=q1.callid) as Callerid,agent,TIMEDIFF((SELECT DATE_FORMAT( TIME,  '%H:%i:%s' ) from queue_log where callid=q1.callid and (event='COMPLETECALLER' or event='COMPLETEAGENT')) ,(SELECT DATE_FORMAT( TIME,  '%H:%i:%s' ) from queue_log where callid=q1.callid and event='CONNECT')) as Talk_Time,DATE_FORMAT( TIME,  '%d-%m-%Y %H:%i:%s' ) as time from queue_log as q1 where event='CONNECT' and callid=q1.callid order by time desc";
 
    $query = $this->db->query($sql);
    return $query->result_array();
 
 }
+
+
+function dialerReport(){ 
+
+$sql = "select * from predictve";
+
+   $query = $this->db->query($sql);
+   return $query->result_array();
+
+} 
+function recordReport(){ 
+
+$sql = "select calldate,clid,src,dst,disposition,sec_to_time(duration) as b from cdr where uniqueid!='' and lastdata like '%SIP/%' order by calldate desc";
+
+   $query = $this->db->query($sql);
+   return $query->result_array();
+
+} 
+
+function ivrReport(){ 
+
+$sql = "";
+
+   $query = $this->db->query($sql);
+   return $query->result_array();
+
+} 
 
 }
 
