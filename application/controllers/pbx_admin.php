52d1832afd5ae7839b9c70167afddc791f9adcde
kumuthavalli <kumuthavalli@tevatel.com>
2013-07-26 10:42:02 +0530

    changes in pbx_admin file

--- application/controllers/pbx_admin.php
+++ application/controllers/pbx_admin.php
@@ -6,13 +6,32 @@ class Pbx_admin extends CI_Controller {
 	      parent::__construct();
 	      $this->load->helper(array('url','form'));
 	      $this->load->model('user','user',TRUE);
+		  $this->load->library('form_validation');
 
 	}
 	
 	function add_extension()
 	{
-		$this->load->view('extension_view.php');
+		$this->load->view('extension_view');
 	}
+	
+	function insert_extension()
+	{
+		
+		$this->form_validation->set_rules('ext', 'Extension', 'trim|required|numeric|xss_clean');
+		$this->form_validation->set_rules('tv_password', 'Password', 'trim|required|xss_clean|callback_check_database');
+		
+			if ($this->form_validation->run() == FALSE)
+				{
+					$this->load->view('extension_view');
+				}
+			else
+				{
+					redirect('extension_list');
+				}
+		
+	}
+	
 	
 	function follow_me()
 	{
--- application/views/extension_view.php
+++ application/views/extension_view.php
@@ -13,53 +13,52 @@ function foo(isChecked){
 <div class="search-container">
 
 <div  class="slide-up-key" style="display: none;">
-<?php
-	$controller_name = "summary/";
-	$attributes = array('class' => '',
-			'id' => '', 'name' => '');
-	echo form_open($controller_name, $attributes);
-?>
- 
-</form>  
 </div>
 </div>
 <div class="agent-summary-list">
+
+<?php echo validation_errors(); ?>
+
+<?php echo form_open('pbx_admin/insert_extension"'); ?>
+
+
   <table class="global-table-style">
     <tr>
       <th colspan="2">Adding extension</th>
     </tr>
    
     <tr>
-	<td>*SIP Extension:</td> <td><input type="text" name="ext"></td>
+	<td>*SIP Extension:</td> <td><input type="text" name="ext" value="<?php echo set_value('ext'); ?>"></td>
     </tr>
     <tr>
-	<td>*Display Name:</td> <td><input type="text" name="name"></td>
+	<td>*Display Name:</td> <td><input type="text" name="name" value="<?php echo set_value('name'); ?>"></td>
     </tr>
     <tr>
-	<td>*Secret:</td> <td><input type="text" name="secret"></td>
+	<td>*Secret:</td> <td><input type="text" name="secret" value="<?php echo set_value('secret'); ?>"></td>
     </tr>
     <tr>
-	<td>Call Group:	</td> <td><input type="text" name="call_group"></td>
+	<td>Call Group:	</td> <td><input type="text" name="call_group" value="<?php echo set_value('call_group'); ?>"></td>
     </tr>
     <tr>
-	<td>Call pickup Group:</td> <td><input type="text" name="pickup_group"></td>
+	<td>Call pickup Group:</td> <td><input type="text" name="pickup_group" value="<?php echo set_value('pickup_group'); ?>"></td>
     </tr>
     <tr>
-	<td>Mailbox:</td> <td><input  type="checkbox" name="mail" id="mail" onchange="foo(this.checked);"/>Enable</td>
+	<td>Mailbox:</td> <td><input  type="checkbox" name="mail" id="mail" onchange="foo(this.checked);" value="<?php echo set_value('mail'); ?>"/>Enable</td>
     </tr>
     <tr>
-	<td>Email id:</td> <td><input type="text" name="mailid" disabled="disabled" ></td>
+	<td>Email id:</td> <td><input type="text" name="mailid" id="mailid" disabled="disabled" value="<?php echo set_value('mailid'); ?>" ></td>
     </tr>	
 	<tr>
-	<td>Password</td> <td><input type="text" name="password" disabled="disabled"></td>
+	<td>Password</td> <td><input type="text" name="password" id="password" disabled="disabled" value="<?php echo set_value('password'); ?>"></td>
     </tr>
 	<tr>
-	<td>Call waiting:</td> <td><input type="text" name="waiting"></td>
+	<td>Call waiting:</td> <td><input type="text" name="waiting" value="<?php echo set_value('waiting'); ?>"></td>
     </tr>
   </table>
 <div align="center">  
   <input type="submit" name="insert" value="Insert">
   <input type="reset" name="reset" value="Reset">
 </div>
+</form>
   </div>
 <?php include "footer.php";?>
