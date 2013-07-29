8936f64bc34ced2c7d8e84b0490876d64423dc6a
Arulprakash <arulprakash@tevatel.com>
2013-07-29 09:49:41 +0530

    report content controller

--- application/controllers/report_content.php
+++ application/controllers/report_content.php
@@ -5,6 +5,7 @@ if (!defined('BASEPATH'))
 
 class Report_content extends CI_Controller {
 
+<<<<<<< Updated upstream
     function __construct() {
         parent::__construct();
         $this->load->helper(array('url', 'form'));
@@ -71,5 +72,58 @@ class Report_content extends CI_Controller {
     }
 
 }
+=======
+	 function __construct() {
+	      parent::__construct();
+	      $this->load->helper(array('url','form'));
+		  $this->load->library("pagination");
+	      $this->load->model('report','report',TRUE);
+		  $this->load->model('global_pagination','global_pagination',TRUE);
+
+	}
+	
+	public function ivr()
+	{
+		
+		
+	}
+	
+	public function did()
+	{		
+		
+		$page_url 		=	base_url() . "index.php/report_content/did";
+		$total_users 		=	$this->report->did_count();
+		$result_page		=	$this->global_pagination->index($page_url,$total_users);
+		$data['did_report'] = $this->report->didReport(3,$result_page);
+		$data['links']	= $this->pagination->create_links();
+		$this->load->view('did_report',$data);
+	}
+	
+	public function queue()
+	{		
+		$data['queue_report'] = $this->report->queueReport();
+		$this->load->view('queue_report',$data);
+	}
+	
+	public function outbound()
+	{		
+		$data['outbound_report'] = $this->report->outboundReport();
+		$this->load->view('outbound_report',$data);
+	}	
+	
+	public function predictive()
+	{
+		$data['predictive_report'] = $this->report->dialerReport();
+		$this->load->view('predictive_report',$data);
+	}
+	
+	public function record()
+	{
+		$data['record_report'] = $this->report->recordReport();
+		$this->load->view('record_report',$data);
+	}
+	
+} 
+>>>>>>> Stashed changes
 ?>
 
