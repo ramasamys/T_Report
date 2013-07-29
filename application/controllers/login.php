b382f616896448d395d1f4b056384fce42022f17
rakesh <rakesh@tevatel.com>
2013-07-29 13:24:38 +0530

    que pass function

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -48,7 +48,7 @@ class Login extends CI_Controller {
 
         if (!empty($result)) {
         $timestamp = sha1(uniqid(5));
-            $this->user->insertlogindetail($result[0]['username'], sha1(uniqid(5)));
+            $this->user->insertlogindetail($result[0]['username'], $timestamp);
             $user_details = array();
             foreach ($result as $row) :
                 $user_details = array(
@@ -107,7 +107,19 @@ class Login extends CI_Controller {
             redirect('login/logout');
         }
     }
-
+    function agentpause(){
+       if ($this->session->userdata('logged_in')) {
+       	       
+            $this->user->agentpausefun();
+            
+        } else {
+        	
+            redirect('login/logout');
+            
+        }	    
+    	    
+	    
+    }
     public function logout() {
         $this->user->loggedout();
         $this->session->sess_destroy();
