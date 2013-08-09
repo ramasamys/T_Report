ca7784e76558a10bed84e881a0aedff2cd8b5901
Ram <ramasamy@tevatel.com>
2013-08-09 17:32:54 +0530

    login error msg

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -12,84 +12,60 @@ class Login extends CI_Controller {
         $this->load->model('user', 'user', TRUE);
     }
 
-    public function index() { 
+    public function index() {
         $this->load->view('login_form');
     }
 
     function verifyLogin() {
-		
-		$username = $this->input->post('tv_username');
-		$password = $this->input->post('tv_password');
-	if(($username == "" || $username == NULL) && ($password == "" || $password == NULL))
-	{
-		$this->load->view('login_form');
-	}
-	else if($username == "" || $username == NULL)
-	{
-			$this->form_validation->set_rules('tv_username', 'Username', 'callback_username_check');
-		       if ($this->form_validation->run() == FALSE) 
-				{
-					$this->load->view('login_form');
-				}
-	}
-	
-	else if($password == "" || $password == NULL)
-	{
-		 $this->form_validation->set_rules('tv_password', 'Password', 'callback_password_check');
 
-        if ($this->form_validation->run() == FALSE) {
-            $this->load->view('login_form');
-        }
-	}
-	else{
-		$this->form_validation->set_rules('tv_username', 'Username', 'trim|required|min_length[4]|max_length[12]|xss_clean');
-        $this->form_validation->set_rules('tv_password', 'Password', 'trim|required|xss_clean|callback_check_database');
+        $username = $this->input->post('tv_username');
+        $password = $this->input->post('tv_password');
+        if (empty($username) && empty($password)) {
+            redirect();
+        } else if (empty($username)) {
+            $this->form_validation->set_rules('tv_username', 'Username', 'callback_username_check');
+            if ($this->form_validation->run() == FALSE) {
+                $this->load->view('login_form');
+            }
+        } else if (empty($password)) {
+            $this->form_validation->set_rules('tv_password', 'Password', 'callback_password_check');
 
-        if ($this->form_validation->run() == FALSE) {
-            $this->load->view('login_form');
+            if ($this->form_validation->run() == FALSE) {
+                $this->load->view('login_form');
+            }
         } else {
-            $this->checkSession();
+            $this->form_validation->set_rules('tv_username', 'Username', 'trim|required|min_length[4]|max_length[12]|xss_clean');
+            $this->form_validation->set_rules('tv_password', 'Password', 'trim|required|xss_clean|callback_check_database');
+
+            if ($this->form_validation->run() == FALSE) {
+                $this->load->view('login_form');
+            } else {
+                $this->checkSession();
+            }
         }
-		}
     }
-	
-	public function username_check($str)
-	{
-		if ($str == '' || $str == NULL)
-		{
-			$this->form_validation->set_message('username_check', 'The username field is empty.');
-			return FALSE;
-		}
-		else
-		{
-			return TRUE;
-		}
-	}
-	
-	public function password_check($str)
-	{
-		if ($str == '' || $str == NULL)
-		{
-			$this->form_validation->set_message('password_check', 'The Password field is empty.');
-			return FALSE;
-		}
-		else
-		{
-			return TRUE;
-		}
-	}
-	
-	
+
+    public function username_check($str) {
+        if ($str == '' || $str == NULL) {
+            $this->form_validation->set_message('username_check', 'The username field is empty.');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+    public function password_check($str) {
+        if ($str == '' || $str == NULL) {
+            $this->form_validation->set_message('password_check', 'The Password field is empty.');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
     function checkSession() {
-	$this->load->helper('cookie');
         $sessionValues = $this->session->userdata('logged_in');
-		
-		if( $this->input->post("rememberme") == 'on')
-                  {
-		      setcookie("username", $sessionValues['username'], time() + 360000, "/");
-		      setcookie("password", $sessionValues['password'], time() + 360000, "/");
-		   }
-	//	  echo 	$sessionValues['username']."====>".$sessionValues['password'];exit;	   
+ 
         if (!empty($sessionValues) && $sessionValues['role'] == 'Administrator') {
             $this->load->view('admin_settings');
         }
@@ -97,7 +73,7 @@ class Login extends CI_Controller {
             $queueList['queue'] = $this->user->queues();
             $queueList['loggedin'] = $this->user->checkQueue();
             $queueList['pause'] = $this->user->pausestatus();
-	    $this->load->view('agentmain',$queueList);
+            $this->load->view('agentmain', $queueList);
         }
         if (empty($sessionValues)) {
             $this->logout();
@@ -108,7 +84,7 @@ class Login extends CI_Controller {
 
         $username = $this->input->post('username');
         $result = $this->user->checkLogin($username, $password);
-		
+
         if (!empty($result)) {
             $timestamp = sha1(uniqid(5));
             $this->user->insertlogindetail($result[0]['username'], $timestamp);
@@ -118,7 +94,7 @@ class Login extends CI_Controller {
                     'id' => $row['id'],
                     'first_name' => $row['first_name'],
                     'username' => $row['username'],
-					'password' => $password,
+                    'password' => $password,
                     'role' => $row['role'],
                     'sessionId' => $timestamp
                 );
@@ -158,13 +134,13 @@ class Login extends CI_Controller {
 
     function queue_login() {
         if ($this->session->userdata('logged_in')) {
-        
-       // $queue=$_POST['queue'];
-		$this->user->agentQueue();
-       	    $queueList['queue'] = $this->user->queues();
+
+            // $queue=$_POST['queue'];
+            $this->user->agentQueue();
+            $queueList['queue'] = $this->user->queues();
             $queueList['loggedin'] = $this->user->checkQueue();
             $queueList['pause'] = $this->user->pausestatus();
-       $this->load->view('agentmain', $queueList);
+            $this->load->view('agentmain', $queueList);
         } else {
             redirect('login/logout');
         }
@@ -172,7 +148,7 @@ class Login extends CI_Controller {
 
     function agentmaincontrol() {
         if ($this->session->userdata('logged_in')) {
-           
+
 
             $this->load->view('agentmain', $data);
         } else {
@@ -183,10 +159,10 @@ class Login extends CI_Controller {
     function agentpause() {
         if ($this->session->userdata('logged_in')) {
             $this->user->agentpausefun();
-			$queueList['queue'] = $this->user->queues();
+            $queueList['queue'] = $this->user->queues();
             $queueList['loggedin'] = $this->user->checkQueue();
             $queueList['pause'] = $this->user->pausestatus();
-			$this->load->view('agentmain', $queueList);
+            $this->load->view('agentmain', $queueList);
         } else {
 
             redirect('login/logout');
@@ -207,17 +183,15 @@ class Login extends CI_Controller {
         $this->session->sess_destroy();
         redirect();
     }
-    
-    function queue_logout(){
-   
-         $this->user->queueLogout();
-       $queueList['queue'] = $this->user->queues();
-       $queueList['loggedin'] = $this->user->checkQueue();
-       $queueList['pause'] = $this->user->pausestatus();
-       $this->load->view('agentmain', $queueList);
-    
+
+    function queue_logout() {
+
+        $this->user->queueLogout();
+        $queueList['queue'] = $this->user->queues();
+        $queueList['loggedin'] = $this->user->checkQueue();
+        $queueList['pause'] = $this->user->pausestatus();
+        $this->load->view('agentmain', $queueList);
     }
-    
 
 }
 ?>
--- application/views/login_form.php
+++ application/views/login_form.php
@@ -1,47 +1,44 @@
-<?php include "header.php";?>
-   
-	<div class="loginform" >
-		<div class="loginTitle"></div>
+<?php include "header.php"; ?>
+
+<div class="loginform" >
+    <div class="loginTitle"></div>
+
+    <?php if (validation_errors()) { ?>
+        <div id="login_error">
+            <div style="text-align: center;"><? echo validation_errors().'<strong>ERROR:</strong>'; ?></div>
+        </div>
+    <? } ?>                
+
+    <?php
+    $controller_name = "login/verifyLogin";
+    $attributes = array('class' => 'report-login',
+        'id' => 'report-login', 'name' => 'report_login');
+    echo form_open($controller_name, $attributes);
+    ?>
+
+    <p>
+        <label for="user_login">Username <br>
+            <input type="text" name="tv_username" value="<?php echo set_value('tv_username'); ?>" placeholder="username" />
+        </label>
+    </p>
+    <p>
+        <label for="user_pass">Password <br>
+            <input type="password" name="tv_password" value="<?php echo set_value('tv_password'); ?>" placeholder="password" />
+    </p>
+    <p class="forgetmenot">
+        <label for="rememberme">
+            <input id="rememberme" type="checkbox" name="rememberme">
+            Remember Me
+        </label>
+    </p>				
+    <p class="submit">
+
+        <input type="submit" value="Login" class="button-color" />
+
+    </p>
+
+</form>
+</div>
 
 
-<?php //echo validation_errors(); ?>
-        <?php if(validation_errors()){ ?>
-                  <div id="login_error">
-                    <?php echo '<strong>ERROR</strong>:'.validation_errors(); ?>
-                </div>
-      <?  } ?>                
-
-		<?php
-			$controller_name = "login/verifyLogin";
-			$attributes = array('class' => 'report-login',
-					'id' => 'report-login', 'name' => 'report_login');
-			echo form_open($controller_name, $attributes);
-
-		?>
-		
-			    <p>
-			    <label for="user_login">Username <br>
-					<input type="text" name="tv_username" value="<?php  echo set_value('tv_username');?>" placeholder="username" onfocus="javascript: if(this.value == this.defaultValue){ this.value = ''; }"/>
-				</label>
-			    </p>
-				<p>
-					<label for="user_pass">Password <br>
-					<input type="password" name="tv_password" value="<?php echo set_value('tv_password');?>" placeholder="password"  onclick="if(this.value == this.defaultValue) this.value = ''"/>
-				</p>
-<p class="forgetmenot">
-<label for="rememberme">
-<input id="rememberme" type="checkbox" name="rememberme">
-Remember Me
-</label>
-</p>				
-				<p class="submit">
-
-				<input type="submit" value="Login" class="button-color" />
-				
-				</p>
-			
-		</form>
-	</div>
-
-	
-<?php include "footer.php";?>
+<?php include "footer.php"; ?>
